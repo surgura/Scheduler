c06b11155a4d01a92ad82e7643882e25b3c358e4
Aart Stuurman <aartstuurman@hotmail.com>
2018-08-26 22:38:14 +0200

    first loop

--- include/scheduler/scheduler.h
+++ include/scheduler/scheduler.h
@@ -7,4 +7,7 @@ typedef struct
     
 } scd_scheduler;
 
-void scd_scd_add_conditional(scd_conditional* cond, void(*task)(void* user_data));
+void scd_scd_construct(scd_scheduler* scheduler);
+void scd_scd_run(scd_scheduler* scheduler);
+void scd_scd_add(scd_scheduler* scheduler, void(*task)(void* user_data), void* user_data);
+void scd_scd_add_conditional(scd_scheduler* scheduler, scd_conditional* cond, void(*task)(void* user_data), void* user_data);
--- test/src/main.c
+++ test/src/main.c
@@ -1,6 +1,6 @@
 #include "scheduler/scheduler.h"
-#include <unistd.h>
 
+scd_scheduler scheduler;
 scd_conditional cond1;
 
 void interrupt1()
@@ -8,17 +8,27 @@ void interrupt1()
 
 }
 
-void fiber1()
+void read_udp_messages(void* user_data);
+
+void on_send_done(void* user_data)
 {
-    sleep(100); // work
+    // TODO read data
+    read_udp_messages(0);
+}
+
+void read_udp_messages(void* user_data)
+{
     scd_cnd_construct(&cond1);
-    // set iterrupt routine
-    // enable
+    // TODO set iterrupt routine
+    // TODO enable
+    scd_scd_add_conditional(&scheduler, &cond1, on_send_done, 0);
 }
 
 int main()
 {
-    scd_scheduler scheduler;
+    scd_scd_construct(&scheduler);
+    scd_scd_add(&scheduler, read_udp_messages, 0);
+    scd_scd_run(&scheduler);
 
     return 0;
 }
